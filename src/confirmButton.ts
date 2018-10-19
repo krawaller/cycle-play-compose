0264fb8b724395e299a2563d03fcb240ce5410bc
David <david.waller@svenskaspel.se>
2018-10-19 22:26:08 +0200

    naked button

--- src/confirmButton.ts
+++ src/confirmButton.ts
@@ -1,52 +1,72 @@
 import {span, button, MainDOMSource} from '@cycle/dom';
-import {StateSource} from '@cycle/state';
+import {StateSource, Reducer, withState} from '@cycle/state';
 import xstream, {Stream} from 'xstream';
 
-type ConfirmButtonSources = {DOM: MainDOMSource, state: StateSource<boolean>};
 
-type Action = 'DISABLE' | 'ENABLE' | 'MAYBE' | 'CANCEL' | 'CONFIRM';
 
-function intent(sources: ConfirmButtonSources) {
+type ConfirmState = 'disabled' | 'areyousure' | 'waiting';
+
+type NakedConfirmButtonSources = {DOM: MainDOMSource, state: StateSource<ConfirmState>};
+
+function intent(sources: NakedConfirmButtonSources) {
+  return {
+    maybe$: sources.DOM.select('.maybe').events('click'),
+    cancel$: sources.DOM.select('.cancel').events('click'),
+    confirm$: sources.DOM.select('.confirm').events('click'),
+  };
+}
+
+function reducer({maybe$, cancel$, confirm$}): Stream<Reducer<ConfirmState>> {
   return xstream.merge(
-    sources.state.stream.map(i=> i ? 'DISABLE' : 'ENABLE'),
-    sources.DOM.select('.maybe').events('click').mapTo('MAYBE'),
-    sources.DOM.select('.cancel').events('click').mapTo('CANCEL'),
-    sources.DOM.select('.confirm').events('click').mapTo('CONFIRM'),
-  ) as Stream<Action>;
+    maybe$.mapTo(() => 'areyousure'),
+    cancel$.mapTo(() => 'waiting'),
+    confirm$.mapTo(() => 'waiting'),
+  ) as Stream<Reducer<ConfirmState>>;
 }
 
-type State = 'disabled' | 'areyousure' | 'waiting';
-
-function model(action$: Stream<Action>): Stream<State>{
-  return action$.map( v => 
-    v === 'DISABLE' ? 'disabled'
-    : v === 'MAYBE' ? 'areyousure'
-    : 'waiting' // because CANCEL, CONFIRM and ENABLE all means we go to waiting mode
-  );
-}
-
-function view(state$: Stream<State>) {
+function view(state$: Stream<ConfirmState>) {
   return state$.map(state => {
     return span('.child', [
       state === 'areyousure' ? span('.confirmapp',[
-        button('.cancel','Cancel'),
+        button('.cancel', 'Cancel'),
         button('.confirm','Confirm')
       ]) : span('.confirmapp',[
-        '','', // prevents DOM driver bug
         button('.maybe',{attrs: {disabled: state === 'disabled'}}, 'Submit')
       ])
     ]);
   });
 }
 
-export default (sources: ConfirmButtonSources) => {
+export function NakedConfirmButton (sources: NakedConfirmButtonSources) {
 
-  const action$ = intent(sources);
-  const state$ = model(action$);
-  const vtree$ = view(state$);
+  const actions = intent(sources);
+
+  const vtree$ = view(sources.state.stream);
 
   return {
     DOM: vtree$,
-    submit$: action$.filter(i => i === 'CONFIRM').mapTo(undefined)
+    state: reducer(actions),
+    submit$: actions.confirm$
   };
 };
+
+type ConfirmButtonSources = NakedConfirmButtonSources & { state: StateSource<boolean>, confirmState: StateSource<ConfirmState> }
+
+const ConfirmButton = withState(function(sources: ConfirmButtonSources){
+  const nakedSinks = NakedConfirmButton({
+    ...sources,
+    state: sources.confirmState
+  });
+
+  const disabledReducer$ = sources.state.stream.map(disabled => (s) => disabled ? 'disabled' : 'waiting');
+
+  return {
+    ...nakedSinks,
+    confirmState: xstream.merge(
+      nakedSinks.state,
+      disabledReducer$
+    )
+  };
+}, 'confirmState');
+
+export default ConfirmButton;
