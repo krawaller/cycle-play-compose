1486592ef9fb04fad3f26a9d578c0a369658ee44
David Waller <dwaller@apple.com>
2020-05-23 07:18:09 +0200

    x days ago

--- src/stats/stats.ts
+++ src/stats/stats.ts
@@ -1,5 +1,6 @@
 import { div, h1 } from "@cycle/dom";
 import { StatsSources, StatsSinks } from "./stats.types";
+import { formatDistanceToNow } from "date-fns";
 
 const box = (label: string, count: number) =>
   div(".statistic", [div(".number", [count]), div(".description", [label])]);
@@ -11,6 +12,11 @@ export function Stats(sources: StatsSources): StatsSinks {
       : state.data
       ? div([
           h1(".title", [state.country]),
+          div(".ago", [
+            "updated ",
+            formatDistanceToNow(new Date(state.data.Date)),
+            " ago",
+          ]),
           div(".statsContainer", [
             box("Confirmed", state.data.Confirmed),
             box("Deaths", state.data.Deaths),
